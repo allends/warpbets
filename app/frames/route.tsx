38eff2b3ad8581f48eae93ad3161692035a44fec
Allen <allen.davisswing@gmail.com>
2024-09-25 12:41:05 -0400

    update to use image url

--- app/frames/route.tsx
+++ app/frames/route.tsx
@@ -11,15 +11,16 @@ const frameHandler = frames(async (ctx) => {
     : ctx.state.counter;
 
   return {
-    image: (
-      <div tw="flex flex-col">
-        <div tw="flex">frames.js starter</div>
-        {ctx.message?.inputText && (
-          <div tw="flex">{`Input: ${ctx.message.inputText}`}</div>
-        )}
-        <div tw="flex">Counter {counter}</div>
-      </div>
-    ),
+    image: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb.jpg',
+    // image: (
+    //   <div tw="flex flex-col">
+    //     <div tw="flex">frames.js starter</div>
+    //     {ctx.message?.inputText && (
+    //       <div tw="flex">{`Input: ${ctx.message.inputText}`}</div>
+    //     )}
+    //     <div tw="flex">Counter {counter}</div>
+    //   </div>
+    // ),
     textInput: "Say something",
     buttons: [
       <Button action="post" target={{ pathname: "/", query: { op: "+" } }}>
